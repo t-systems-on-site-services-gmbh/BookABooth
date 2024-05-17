7918fea5e9544843dd5fef710c89bb5207ed88cf
gricklom <gbette@gmail.com>
2024-05-17 20:30:57 +0200

    Fixes #3 enable/disable reservation

--- src/main/webapp/app/entities/system/system.service.ts
+++ src/main/webapp/app/entities/system/system.service.ts
@@ -5,64 +5,12 @@ import { type ISystem } from '@/shared/model/system.model';
 const baseApiUrl = 'api/systems';
 
 export default class SystemService {
-  public find(id: number): Promise<ISystem> {
-    return new Promise<ISystem>((resolve, reject) => {
-      axios
-        .get(`${baseApiUrl}/${id}`)
-        .then(res => {
-          resolve(res.data);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
-  }
-
   public retrieve(): Promise<any> {
     return new Promise<any>((resolve, reject) => {
       axios
         .get(baseApiUrl)
         .then(res => {
           resolve(res);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
-  }
-
-  public delete(id: number): Promise<any> {
-    return new Promise<any>((resolve, reject) => {
-      axios
-        .delete(`${baseApiUrl}/${id}`)
-        .then(res => {
-          resolve(res);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
-  }
-
-  public create(entity: ISystem): Promise<ISystem> {
-    return new Promise<ISystem>((resolve, reject) => {
-      axios
-        .post(`${baseApiUrl}`, entity)
-        .then(res => {
-          resolve(res.data);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
-  }
-
-  public update(entity: ISystem): Promise<ISystem> {
-    return new Promise<ISystem>((resolve, reject) => {
-      axios
-        .put(`${baseApiUrl}/${entity.id}`, entity)
-        .then(res => {
-          resolve(res.data);
         })
         .catch(err => {
           reject(err);
