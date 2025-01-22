ac50980e376dd7516a61959ff6e2f92f57c3a384
gricklom <gerrit.bette@t-systems.com>
2025-01-22 11:31:24 +0100

    feature 6 - remove "Stand buchen" from nav-bar

--- src/main/webapp/app/core/jhi-navbar/jhi-navbar.vue
+++ src/main/webapp/app/core/jhi-navbar/jhi-navbar.vue
@@ -30,13 +30,6 @@
             <span>Ausstellerliste</span>
           </span>
         </b-nav-item>
-
-        <b-nav-item to="/bookabooth" exact>
-          <span>
-            <font-awesome-icon icon="receipt" />
-            <span>Stand buchen</span>
-          </span>
-        </b-nav-item>
         <b-nav-item-dropdown
           right
           id="entity-menu"
