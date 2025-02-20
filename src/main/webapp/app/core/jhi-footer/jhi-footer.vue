ba37edafb1763013b9b03991921af8cab0d80ea1
gricklom <gerrit.bette@t-systems.com>
2025-02-20 14:55:39 +0100

    + add link to T-Systems onsite to footer

--- src/main/webapp/app/core/jhi-footer/jhi-footer.vue
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="footer" class="footer">
-    <a href="https://www.jade-hs.de/impressum/" target="_blank">Impressum</a> |
-    <a href="https://www.jade-hs.de/datenschutz/" target="_blank">Datenschutzerklärung</a> |
-    <a href="ausstellerinfo">Ausstellerinformationen</a>
+  <div class="footer d-flex flex-row justify-content-between">
+    <div>
+      <a href="https://www.jade-hs.de/impressum/" target="_blank">Impressum</a> |
+      <a href="https://www.jade-hs.de/datenschutz/" target="_blank">Datenschutzerklärung</a> |
+      <a href="ausstellerinfo">Ausstellerinformationen</a>
+    </div>
+    <div>powered by <a href="https://www.t-systems-onsite.de" target="_blank">T-Systems on site services GmbH</a></div>
   </div>
 </template>
 
