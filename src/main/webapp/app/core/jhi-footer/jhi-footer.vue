64243339aa6d1d9a4f27ecd1ec058550d9d9842e
Jan-Claas Uphoff <jan-claas.uphoff@t-systems.com>
2025-02-21 17:17:32 +0100

    color magenta for T-Systems footer

--- src/main/webapp/app/core/jhi-footer/jhi-footer.vue
+++ src/main/webapp/app/core/jhi-footer/jhi-footer.vue
@@ -5,7 +5,9 @@
       <a href="https://www.jade-hs.de/datenschutz/" target="_blank">Datenschutzerklärung</a> |
       <a href="ausstellerinfo">Ausstellerinformationen</a>
     </div>
-    <div>powered by <a href="https://www.t-systems-onsite.de" target="_blank">T-Systems on site services GmbH</a></div>
+    <div>
+      powered by <a style="color: #e20074" href="https://www.t-systems-onsite.de" target="_blank">T-Systems on site services GmbH</a>
+    </div>
   </div>
 </template>
 
