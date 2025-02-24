73ed65bc61fb4e12ea1f74ea98f3ee6e17b4a927
gricklom <gerrit.bette@t-systems.com>
2025-02-24 13:50:28 +0100

    feature 50 + add findAllEnriched to location repository

--- src/main/java/de/tsystems/onsite/bookabooth/repository/LocationRepository.java
+++ src/main/java/de/tsystems/onsite/bookabooth/repository/LocationRepository.java
@@ -1,6 +1,7 @@
 package de.tsystems.onsite.bookabooth.repository;
 
 import de.tsystems.onsite.bookabooth.domain.Location;
+import java.util.List;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,14 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface LocationRepository extends JpaRepository<Location, Long> {}
+public interface LocationRepository extends JpaRepository<Location, Long> {
+    @Query(
+        value = "SELECT l.id, l.location, l.url, COUNT(DISTINCT b.id) amount, COUNT(DISTINCT bk.id) booked " +
+        "FROM location l " +
+        "LEFT JOIN booth b ON b.location_id = l.id " +
+        "LEFT JOIN booking bk ON bk.booth_id = b.id AND bk.status = 'CONFIRMED' " +
+        "GROUP BY l.id ",
+        nativeQuery = true
+    )
+    List<Object[]> findAllEnriched();
+}
