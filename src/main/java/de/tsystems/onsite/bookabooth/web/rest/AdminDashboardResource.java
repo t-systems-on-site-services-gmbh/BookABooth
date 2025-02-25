d6e7cf20eafbb3a0eeb50aed1fee0dfa56f163c2
Jan-Claas Uphoff <jan-claas.uphoff@t-systems.com>
2025-02-25 14:28:16 +0100

    dashboard checklist service added

--- src/main/java/de/tsystems/onsite/bookabooth/web/rest/AdminDashboardResource.java
+++ src/main/java/de/tsystems/onsite/bookabooth/web/rest/AdminDashboardResource.java
@@ -1,10 +1,13 @@
 package de.tsystems.onsite.bookabooth.web.rest;
 
+import de.tsystems.onsite.bookabooth.domain.Company;
+import de.tsystems.onsite.bookabooth.repository.CompanyRepository;
+import de.tsystems.onsite.bookabooth.service.CompanyService;
 import de.tsystems.onsite.bookabooth.service.dto.AdminChecklistDTO;
-import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminDashboardResource {
 
     private final Logger log = LoggerFactory.getLogger(AdminDashboardResource.class);
+    private CompanyRepository companyRepository;
+    private CompanyService companyService;
 
+    public AdminDashboardResource(CompanyRepository companyRepository, CompanyService companyService) {
+        this.companyRepository = companyRepository;
+        this.companyService = companyService;
+    }
+
+    @GetMapping("/checklist")
     public List<AdminChecklistDTO> getChecklist() {
         log.debug("REST request to get checklist");
-        List<AdminChecklistDTO> checklist = new ArrayList<>();
+
+        // all companies
+        List<Company> companies = companyRepository.findAll();
+        List<AdminChecklistDTO> adminChecklist = companyService.getAdminChecklist(companies);
+
         AdminChecklistDTO adminChecklistDTO = new AdminChecklistDTO();
         adminChecklistDTO.setCompanyName("Company Name");
         adminChecklistDTO.setAddress(true);
@@ -24,7 +39,7 @@ public class AdminDashboardResource {
         adminChecklistDTO.setPhoneNumber(true);
         adminChecklistDTO.setCompanyDescription(true);
         adminChecklistDTO.setBooth("Booth");
-        checklist.add(adminChecklistDTO);
+        adminChecklist.add(adminChecklistDTO);
         AdminChecklistDTO adminChecklistDTO2 = new AdminChecklistDTO();
         adminChecklistDTO2.setCompanyName("Company Name 2");
         adminChecklistDTO2.setAddress(true);
@@ -32,8 +47,8 @@ public class AdminDashboardResource {
         adminChecklistDTO2.setPhoneNumber(true);
         adminChecklistDTO2.setCompanyDescription(true);
         adminChecklistDTO2.setBooth("Booth");
-        checklist.add(adminChecklistDTO);
+        adminChecklist.add(adminChecklistDTO);
 
-        return checklist;
+        return adminChecklist;
     }
 }
