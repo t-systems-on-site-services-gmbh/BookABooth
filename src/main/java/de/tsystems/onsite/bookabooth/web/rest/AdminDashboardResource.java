ecc42bce4b52599ccfad090593b1ae8649650a58
Jan-Claas Uphoff <jan-claas.uphoff@t-systems.com>
2025-02-25 19:08:36 +0100

    adminChecklist completed

--- src/main/java/de/tsystems/onsite/bookabooth/web/rest/AdminDashboardResource.java
+++ src/main/java/de/tsystems/onsite/bookabooth/web/rest/AdminDashboardResource.java
@@ -7,6 +7,7 @@ import de.tsystems.onsite.bookabooth.service.dto.AdminChecklistDTO;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,30 +25,28 @@ public class AdminDashboardResource {
         this.companyService = companyService;
     }
 
+    /**
+     * {@code GET  /checklist} : get the checklist.
+     *
+     * @return the {@link List} of entities.
+     */
     @GetMapping("/checklist")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public List<AdminChecklistDTO> getChecklist() {
         log.debug("REST request to get checklist");
 
         // all companies
         List<Company> companies = companyRepository.findAll();
+
         List<AdminChecklistDTO> adminChecklist = companyService.getAdminChecklist(companies);
 
-        AdminChecklistDTO adminChecklistDTO = new AdminChecklistDTO();
-        adminChecklistDTO.setCompanyName("Company Name");
-        adminChecklistDTO.setAddress(true);
-        adminChecklistDTO.setLogo(true);
-        adminChecklistDTO.setPhoneNumber(true);
-        adminChecklistDTO.setCompanyDescription(true);
-        adminChecklistDTO.setBooth("Booth");
-        adminChecklist.add(adminChecklistDTO);
-        AdminChecklistDTO adminChecklistDTO2 = new AdminChecklistDTO();
-        adminChecklistDTO2.setCompanyName("Company Name 2");
-        adminChecklistDTO2.setAddress(true);
-        adminChecklistDTO2.setLogo(true);
-        adminChecklistDTO2.setPhoneNumber(true);
-        adminChecklistDTO2.setCompanyDescription(true);
-        adminChecklistDTO2.setBooth("Booth");
-        adminChecklist.add(adminChecklistDTO2);
+        adminChecklist.sort((o1, o2) -> {
+            if (o1.getAddress() && o1.getPhoneNumber()) {
+                return 0;
+            } else {
+                return -1;
+            }
+        });
 
         return adminChecklist;
     }
