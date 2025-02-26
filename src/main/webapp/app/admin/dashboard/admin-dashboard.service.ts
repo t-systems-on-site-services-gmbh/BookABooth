07eab34e45bf412d4ecbe35d7027569658bc4a6e
Jan-Claas Uphoff <jan-claas.uphoff@t-systems.com>
2025-02-26 16:29:06 +0100

    pom modified, downloadExcel added on admin dashboard

--- src/main/webapp/app/admin/dashboard/admin-dashboard.service.ts
+++ src/main/webapp/app/admin/dashboard/admin-dashboard.service.ts
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 import type { IAdminChecklist } from '@/shared/model/admin-checklist.model';
 
-const baseApiUrl = 'api/admin-dashboard/checklist';
+const checklistUrl = 'api/admin-dashboard/checklist';
+const excelUrl = 'api/bookings/downloadexcel';
 
 export default class AdminDashboardService {
   public checklist(): Promise<IAdminChecklist> {
     return new Promise<IAdminChecklist>((resolve, reject) => {
       axios
-        .get(`${baseApiUrl}`)
+        .get(`${checklistUrl}`)
         .then(res => {
           resolve(res.data);
         })
@@ -16,5 +17,17 @@ export default class AdminDashboardService {
           reject(err);
         });
     });
+  }
+
+  public async downloadExcel(): Promise<void> {
+    debug;
+    const response = await axios.get(`${excelUrl}`, { responseType: 'blob' });
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'report.xlsx');
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
   }
 }
