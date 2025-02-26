897a95e1579d1fb4f8d1b9de2a3e5c4aa289ae15
Jan-Claas Uphoff <jan-claas.uphoff@t-systems.com>
2025-02-26 16:29:06 +0100

    pom modified, downloadExcel added on admin dashboard

--- src/main/webapp/app/admin/dashboard/admin-dashboard.component.ts
+++ src/main/webapp/app/admin/dashboard/admin-dashboard.component.ts
@@ -4,6 +4,7 @@ import type { ILocation } from '@/shared/model/location.model';
 import LocationService from '@/entities/location/location.service';
 import type { IAdminChecklist } from '@/shared/model/admin-checklist.model';
 import AdminDashboardService from '@/admin/dashboard/admin-dashboard.service';
+import axios from 'axios';
 
 export default defineComponent({
   compatConfig: { MODE: 3 },
@@ -33,14 +34,24 @@ export default defineComponent({
     return {
       locations,
       checklists,
+      adminDashboardService,
     };
   },
   mounted() {
     this.init();
   },
   methods: {
-    init(): void {
-      //this.items.push({ name: 'Greeting', value: 'Moin World' });
+    init(): void {},
+    async downloadExcel(): void {
+      const excelUrl = 'api/bookings/downloadexcel';
+      const response = await axios.get(`${excelUrl}`, { responseType: 'blob' });
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'report.xlsx');
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
     },
   },
 });
