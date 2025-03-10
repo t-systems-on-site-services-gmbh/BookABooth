9fc1b33a1d4b537ce5d884a90c855dce9ba88364
Cramer <romina.cramer@t-systems.com>
2025-03-10 15:56:43 +0100

    adjusted logo source for navbar and spelling in register.vue

--- src/main/webapp/app/core/jhi-navbar/jhi-navbar.vue
+++ src/main/webapp/app/core/jhi-navbar/jhi-navbar.vue
@@ -183,7 +183,7 @@
 
 .logo-img {
   height: 100%;
-  background: url('/content/images/jhs-logo.png') no-repeat center center;
+  background: url('/content/images/jadehs-logo.png') no-repeat center center;
   background-size: contain;
   width: 100%;
   filter: drop-shadow(0 0 0.05rem white);
