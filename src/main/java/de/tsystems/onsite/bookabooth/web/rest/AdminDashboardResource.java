ef9b94f9ba4cfbf387d110cc3626af699dac255f
Jan-Claas Uphoff <jan-claas.uphoff@t-systems.com>
2025-03-29 00:28:24 +0100

    excel billing download improvements - Booth number with location name instead of id - download filename changed to Buchungen - Address field with text wrapping - refactor: streamline code and improve readability in AdminDashboardService and ExcelService

--- src/main/java/de/tsystems/onsite/bookabooth/web/rest/AdminDashboardResource.java
+++ src/main/java/de/tsystems/onsite/bookabooth/web/rest/AdminDashboardResource.java
@@ -1,23 +1,11 @@
 package de.tsystems.onsite.bookabooth.web.rest;
 
-import de.tsystems.onsite.bookabooth.domain.Booking;
-import de.tsystems.onsite.bookabooth.domain.BoothUser;
-import de.tsystems.onsite.bookabooth.domain.Company;
-import de.tsystems.onsite.bookabooth.domain.enumeration.BookingStatus;
-import de.tsystems.onsite.bookabooth.repository.BookingRepository;
-import de.tsystems.onsite.bookabooth.repository.BoothUserRepository;
-import de.tsystems.onsite.bookabooth.repository.CompanyRepository;
 import de.tsystems.onsite.bookabooth.service.AdminDashboardService;
-import de.tsystems.onsite.bookabooth.service.dto.AdminChecklistDTO;
 import de.tsystems.onsite.bookabooth.service.dto.AdminDashboardDTO;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,20 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminDashboardResource {
 
     private final Logger log = LoggerFactory.getLogger(AdminDashboardResource.class);
-    private CompanyRepository companyRepository;
-    private BoothUserRepository boothUserRepository;
-    private BookingRepository bookingRepository;
     private AdminDashboardService AdminDashboardService;
 
-    public AdminDashboardResource(
-        CompanyRepository companyRepository,
-        BoothUserRepository boothUserRepository,
-        BookingRepository bookingRepository,
-        AdminDashboardService AdminDashboardService
-    ) {
-        this.companyRepository = companyRepository;
-        this.boothUserRepository = boothUserRepository;
-        this.bookingRepository = bookingRepository;
+    public AdminDashboardResource(AdminDashboardService AdminDashboardService) {
         this.AdminDashboardService = AdminDashboardService;
     }
 
