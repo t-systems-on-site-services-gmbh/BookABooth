7ab7961343a7be9cc08e5fa9ae02154d900215bc
gricklom <gerrit.bette@t-systems.com>
2025-04-14 13:07:22 +0200

    o correct routes in PrivacyPolicyResource

--- src/main/java/de/tsystems/onsite/bookabooth/web/rest/PrivacyPolicyResource.java
+++ src/main/java/de/tsystems/onsite/bookabooth/web/rest/PrivacyPolicyResource.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/privacy-policy")
 public class PrivacyPolicyResource {
 
     private final Logger log = LoggerFactory.getLogger(PrivacyPolicyResource.class);
@@ -30,7 +30,7 @@ public class PrivacyPolicyResource {
      * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new privacy policy, or with status {@code 400 (Bad Request)} if the privacy policy has already an ID.
      * @throws URISyntaxException if the Location URI syntax is incorrect.
      */
-    @PostMapping("/privacy-policy")
+    @PostMapping("")
     public ResponseEntity<PrivacyPolicyDTO> createPrivacyPolicy(@RequestBody PrivacyPolicyDTO privacyPolicyDTO) throws URISyntaxException {
         log.debug("REST request to save PrivacyPolicy : {}", privacyPolicyDTO);
         if (privacyPolicyDTO.getId() != null) {
@@ -45,13 +45,18 @@ public class PrivacyPolicyResource {
      *
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the latest privacy policy, or with status {@code 404 (Not Found)}.
      */
-    @GetMapping("/privacy-policy/latest")
+    @GetMapping("/latest")
     public ResponseEntity<PrivacyPolicyDTO> getLatestPrivacyPolicy() {
         log.debug("REST request to get the latest PrivacyPolicy");
         Optional<PrivacyPolicyDTO> privacyPolicyDTO = privacyPolicyService.findLatestById();
         return privacyPolicyDTO.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    /**
+     * {@code GET  /privacy-policy} : get all privacy policies.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of privacy policies in body.
+     */
     @GetMapping("")
     public List<PrivacyPolicyDTO> getAllPrivacyPolicies() {
         log.debug("REST request to get all privacy policies");
