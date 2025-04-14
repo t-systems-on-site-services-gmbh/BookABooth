5c61265dc6213d5dfe17f24dc37fa762212659af
gricklom <gerrit.bette@t-systems.com>
2025-04-14 14:30:18 +0200

    o fix findLatestPrivacyPolicyById

--- src/main/java/de/tsystems/onsite/bookabooth/repository/PrivacyPolicyRepository.java
+++ src/main/java/de/tsystems/onsite/bookabooth/repository/PrivacyPolicyRepository.java
@@ -2,6 +2,7 @@ package de.tsystems.onsite.bookabooth.repository;
 
 import de.tsystems.onsite.bookabooth.domain.PrivacyPolicy;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PrivacyPolicyRepository extends JpaRepository<PrivacyPolicy, Long> {
     @Query("SELECT p FROM PrivacyPolicy p ORDER BY p.id DESC")
-    Optional<PrivacyPolicy> findLatestPrivacyPolicyById();
+    Optional<PrivacyPolicy> findLatestPrivacyPolicyById(Pageable pageable);
 }
