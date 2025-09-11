50d682173c685cf632867fc39641e8d10caa1966
Jan-Claas Uphoff <jan-claas.uphoff@t-systems.com>
2025-09-11 13:16:29 +0200

    add functionality to download zip of exhibitor logos with confirmed bookings

---
+++ src/main/java/de/tsystems/onsite/bookabooth/service/dto/ExhibitorDTO.java
@@ -0,0 +1,58 @@
+package de.tsystems.onsite.bookabooth.service.dto;
+
+public class ExhibitorDTO {
+
+    private String companyName;
+    private String companyLogo;
+    private String locationName;
+    private String boothTitle;
+    private boolean exhibitorList;
+
+    public ExhibitorDTO(String companyName, String companyLogo, String locationName, String boothTitle, Boolean exhibitorList) {
+        this.companyName = companyName;
+        this.companyLogo = companyLogo;
+        this.locationName = locationName;
+        this.boothTitle = boothTitle;
+        this.exhibitorList = exhibitorList;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyLogo() {
+        return companyLogo;
+    }
+
+    public void setCompanyLogo(String companyLogo) {
+        this.companyLogo = companyLogo;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getBoothTitle() {
+        return boothTitle;
+    }
+
+    public void setBoothTitle(String boothTitle) {
+        this.boothTitle = boothTitle;
+    }
+
+    public Boolean isExhibitorList() {
+        return exhibitorList;
+    }
+
+    public void setExhibitorList(Boolean exhibitorList) {
+        this.exhibitorList = exhibitorList;
+    }
+}
